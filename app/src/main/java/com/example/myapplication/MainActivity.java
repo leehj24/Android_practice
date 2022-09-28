c43996fef7a4019c9aeb2c583b2d66b235e047c9
leehj24 <hyunji4579@gmail.com>
2022-09-28 10:38:35 +0900

    4주차 과제

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,112 +1,55 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    EditText Edit1, Edit2;
-    Button btnAdd, btnSub, btnMul, btnDiv, btnExt;
-    TextView textResult;
-    String num1, num2;
-    Float result;
+    EditText edtUrl;
+    Button btnGo,btnBack;
+    WebView web;
 
-    @Override /*오버라이드 오버로드 차이 알기*/
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("초간단 계산기(수정)");
 
-        Edit1=(EditText) findViewById(R.id.Edit1);
-        Edit2=(EditText) findViewById(R.id.Edit2);
+        edtUrl = (EditText)findViewById(R.id.edtUrl);
+        btnGo = (Button)findViewById(R.id.btnGo);
+        btnBack = (Button)findViewById(R.id.btnBack);
+        web = (WebView)findViewById(R.id.webView1);
 
-        btnAdd=(Button) findViewById(R.id.BtnAdd);
-        btnSub=(Button) findViewById(R.id.BtnSub);
-        btnMul=(Button) findViewById(R.id.BtnMul);
-        btnDiv=(Button) findViewById(R.id.BtnDiv);
-        btnExt=(Button) findViewById(R.id.BtnExt);
+        web.setWebViewClient(new WebViewClient());
 
-        textResult=(TextView) findViewById(R.id.TextResult);
+        WebSettings webset = web.getSettings();
+        webset.setBuiltInZoomControls(true);
+        webset.setJavaScriptEnabled(true);
 
-        btnAdd.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0){
-                num1=Edit1.getText().toString();
-                num2=Edit2.getText().toString();
-                if (num1.trim().equals("")||num2.trim().equals("")){
-                    Toast myToast = Toast.makeText(getApplicationContext(),"입력 값이 비었습니다.", Toast.LENGTH_SHORT);
-                    myToast.show();
-                } else {
-                    result=Float.parseFloat(num1) + Float.parseFloat(num2);
-                    textResult.setText("계산 결과 : " + result.toString());
-                }
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                web.loadUrl(edtUrl.getText().toString());
             }
-        });
+        });// btnGo
 
-        btnSub.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0){
-                num1=Edit1.getText().toString();
-                num2=Edit2.getText().toString();
-                if(num1.trim().equals("")||num2.trim().equals("")){
-                    Toast myToast = Toast.makeText(getApplicationContext(),"입력 값이 비었습니다.", Toast.LENGTH_SHORT);
-                    myToast.show();
-                } else {
-                    result=Float.parseFloat(num1) - Float.parseFloat(num2);
-                    textResult.setText("계산 결과 : " + result.toString());
-                }
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                web.goBack();
             }
-        });
-
-        btnMul.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0){
-                num1=Edit1.getText().toString();
-                num2=Edit2.getText().toString();
-                if (num1.trim().equals("")||num2.trim().equals("")){
-                    Toast myToast = Toast.makeText(getApplicationContext(),"입력 값이 비었습니다.", Toast.LENGTH_SHORT);
-                    myToast.show();
-                } else {
-                    result=Float.parseFloat(num1) * Float.parseFloat(num2);
-                    textResult.setText("계산 결과 : " + result.toString());
-                }
-            }
-        });
-
-        btnDiv.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0){
-                num1=Edit1.getText().toString();
-                num2=Edit2.getText().toString();
-                String zero="0";
-                if(num1.trim().equals("")||num2.trim().equals("")){
-                    Toast myToast=Toast.makeText(getApplicationContext(), "입력 값이 비었습니다.", Toast.LENGTH_SHORT);
-                    myToast.show();
-                } else if(num2.equals(zero)){
-                    Toast myToast = Toast.makeText(getApplicationContext(),"0으로 나눌 수 없습니다.", Toast.LENGTH_SHORT);
-                    myToast.show();
-                } else {
-                    result=Float.parseFloat(num1) / Float.parseFloat(num2);
-                    textResult.setText("계산 결과 : " + result.toString());
-                }
-            }
-        });
-
-        btnExt.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0){
-                num1=Edit1.getText().toString();
-                num2=Edit2.getText().toString();
-                if(num1.trim().equals("")||num2.trim().equals("")){
-                    Toast myToast = Toast.makeText(getApplicationContext(), "입력 값이 비었습니다.", Toast.LENGTH_SHORT);
-                    myToast.show();
-                } else {
-                    result=Float.parseFloat(num1) % Float.parseFloat(num2);
-                    textResult.setText("계산 결과 : " + result.toString());
-                }
-            }
-        });
+        });//btnBack
     }
+
+    class WebViewClient extends android.webkit.WebViewClient{
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }//Web
 }
