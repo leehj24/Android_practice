dd0ce2d9cd94b2137c1edcc50e487d35f943d2bd
gitlab이현지 <2019253099@yonsei.ac.kr>
2022-11-24 12:26:01 +0900

    계산기

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,59 +1,64 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RadioButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("명화 선호도 투표");
+        setTitle("메인 엑티비티");
 
-        final int voteCount[] = new int[9];
-        for (int i = 0; i < 9; i++)
-            voteCount[i] = 0;
-        // 9개의 이미지 버튼 객체 배열
-        ImageView image[] = new ImageView[9];
-        // 9개의 이미지 버튼 ID 배열
-        Integer imageId[] = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5,
-                R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
+        final EditText edtNum1 = findViewById(R.id.edtNum1);
+        final EditText edtNum2 = findViewById(R.id.edtNum2);
+        final RadioGroup rdoGroup = findViewById(R.id.rdoGroup);
+        Button btnNewActivity = findViewById(R.id.btnNewActivity);
 
-        final String imgName[] = {"독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀",
-                "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매",
-                "피아노 레슨", "피아노 앞의 소녀들", "해변에서"};
-
-        for (int i = 0; i < imageId.length; i++) {
-            final int index = i; // 꼭 필요함
-            image[index] = findViewById(imageId[index]);
-            image[index].setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    voteCount[index]++;
-                    Toast.makeText(getApplicationContext(),
-                            imgName[index] + ": 총 " + voteCount[index] + " 표",
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
-        Button btnFinish = (Button) findViewById(R.id.btnResult);
-        btnFinish.setOnClickListener(new View.OnClickListener() {
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-                intent.putExtra("VoteCount", voteCount);
-                intent.putExtra("ImageName", imgName);
-                startActivity(intent);
+
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                switch (rdoGroup.getCheckedRadioButtonId()) {
+                    case R.id.rdoAdd:
+                        intent.putExtra("Calc", "+");
+                        break;
+                    case R.id.rdoSub:
+                        intent.putExtra("Calc", "-");
+                        break;
+                    case R.id.rdoMul:
+                        intent.putExtra("Calc", "*");
+                        break;
+                    case R.id.rdoDiv:
+                        intent.putExtra("Calc", "/");
+                        break;
+                    default:
+                        break;
+                }
+
+                intent.putExtra("Num1",
+                        Integer.parseInt(edtNum1.getText().toString()));
+                intent.putExtra("Num2",
+                        Integer.parseInt(edtNum2.getText().toString()));
+
+                startActivityForResult(intent, 0);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            int hap = data.getIntExtra("Hap", 0);
+            Toast.makeText(getApplicationContext(), "결과 :" + hap,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
