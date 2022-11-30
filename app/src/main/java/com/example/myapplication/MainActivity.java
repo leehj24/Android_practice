6dde396efe2ee3e2fe87d15454a6c4034cdaacb0
gitlab이현지 <2019253099@yonsei.ac.kr>
2022-11-30 11:40:15 +0900

    스피너/영화보여주기

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity {
@@ -11,13 +14,31 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("갤러리 영화 포스터(개선)");
+        setTitle("스피너 테스트");
 
-        final String[] movie = {"쿵푸팬더","짱구는 못말려", "아저씨","아바타", "대부","국가대표","토이스토리3","마당을 나온 암탉", "죽은 시인의 사회", "서유기"};
-        Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+        final String[] movie = { "쿵푸팬더", "짱구는 못말려", "아저씨", "아바타", "대부", "국가대표",
+                "토이스토리3", "마당을 나온 암탉", "죽은 시인의 사회", "서유기" };
 
-        ArrayAdapter<String>adapter;
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,movie);
+        final Integer[] posterID = { R.drawable.mov21, R.drawable.mov22,
+                R.drawable.mov23, R.drawable.mov24, R.drawable.mov25,
+                R.drawable.mov26, R.drawable.mov27, R.drawable.mov28,
+                R.drawable.mov29, R.drawable.mov30 };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, movie);
+
+        Spinner spinner = findViewById(R.id.spinner1);
         spinner.setAdapter(adapter);
-       }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> arg0, View view,
+                                       int position, long id) {
+                ImageView ivPoster = findViewById(R.id.ivPoster);
+                ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ivPoster.setPadding(5, 5, 5, 5);
+                ivPoster.setImageResource(posterID[position]);
+            }
+            public void onNothingSelected(AdapterView<?> arg0) { }
+        });
     }
+}
