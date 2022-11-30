bc39d76423edd91c205549e01b10894ec5ffca83
gitlab이현지 <2019253099@yonsei.ac.kr>
2022-11-30 11:33:54 +0900

    스피너

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,9 @@
 package com.example.myapplication;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.app.AlertDialog;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Gallery;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -21,54 +13,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         setTitle("갤러리 영화 포스터(개선)");
 
-        Gallery gallery = findViewById(R.id.gallery1);
+        final String[] movie = {"쿵푸팬더","짱구는 못말려", "아저씨","아바타", "대부","국가대표","토이스토리3","마당을 나온 암탉", "죽은 시인의 사회", "서유기"};
+        Spinner spinner = (Spinner)findViewById(R.id.spinner1);
 
-        MyGalleryAdapter galAdapter = new MyGalleryAdapter(this);
-        gallery.setAdapter(galAdapter);
+        ArrayAdapter<String>adapter;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,movie);
+        spinner.setAdapter(adapter);
+       }
     }
-
-    public class MyGalleryAdapter extends BaseAdapter {
-
-        Context context;
-        Integer[] posterID = {R.drawable.mov11, R.drawable.mov12,
-                R.drawable.mov13, R.drawable.mov14, R.drawable.mov15,
-                R.drawable.mov16, R.drawable.mov17, R.drawable.mov18,
-                R.drawable.mov19, R.drawable.mov20};
-
-        public MyGalleryAdapter(Context c) { context = c; }
-
-        public int getCount() { return posterID.length; }
-
-        public Object getItem(int arg0) { return null; }
-
-        public long getItemId(int position) { return 0; }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageview = new ImageView(context);
-            imageview.setLayoutParams(new Gallery.LayoutParams(200, 300));
-            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            imageview.setPadding(5, 5, 5, 5);
-            imageview.setImageResource(posterID[position]);
-
-            final int pos = position;
-            imageview.setOnTouchListener(new View.OnTouchListener() {
-                public boolean onTouch(View v, MotionEvent event) {
-                    ImageView ivPoster = findViewById(R.id.ivPoster);
-                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                    ivPoster.setImageResource(posterID[pos]);
-
-                    Toast toast = new Toast(getApplicationContext());
-                    View toastView = View.inflate(getApplicationContext(), R.layout.toast, null);
-                    TextView toastText = toastView.findViewById(R.id.textView1);
-                    toastText.setText("영화큰포스터");
-                    toast.setView(toastView);
-                    toast.show();
-
-                    return false;
-                }
-            });
-
-            return imageview;
-        }
-    }
-}
